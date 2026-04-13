ccc358be8030e22e4a34535ad8adbbd0d8611e4e
Matthew Blackburn <matthewblackbu@gmail.com>
2026-04-13 14:38:19 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,113 @@
+# The plan for this project is to create the best playfair cipher 
+# cracker by analyzing patterns in the ciphertext
+
+# The once piece is real
+
+# T	H	E	O	N
+# P	I	C	S	R
+# A	L	B	D	F
+# G	K	M	Q	U
+# V	W	X	Y	Z
+
+# According to all known laws of aviation, there is no way a bee should be able to fly.
+# Its wings are too small to get its fat little body off the ground.
+# The bee, of course, flies anyway because bees don't care what humans think is impossible.
+# Yellow, black. Yellow, black. Yellow, black. Yellow, black.
+# Ooh, black and yellow!
+# Let's shake it up a little.
+# Barry! Breakfast is ready!
+# Coming!
+# Hang on a second.
+# Hello?
+# Barry?
+# Adam?
+# Can you believe this is happening?
+# I can't.
+# I'll pick you up.
+# Looking sharp.
+# Use the stairs, Your father paid good money for those.
+# Sorry. I'm excited.
+# Here's the graduate.
+# We're very proud of you, son.
+# A perfect report card, all B's.
+# Very proud.
+# Ma! I got a thing going here.
+# You got lint on your fuzz.
+# Ow! That's me!
+# Wave to us! We'll be in row 118,000.
+# Bye!
+# Barry, I told you, stop flying in the house!
+# Hey, Adam.
+# Hey, Barry.
+# Is that fuzz gel?
+# A little. Special day, graduation.
+# Never thought I'd make it.
+# Three days grade school, three days high school.
+# Those were awkward.
+# Three days college. I'm glad I took a day and hitchhiked around The Hive.
+# You did come back different.
+# Hi, Barry. Artie, growing a mustache? Looks good.
+# Hear about Frankie?
+# Yeah.
+# You going to the funeral?
+# No, I'm not going.
+# Everybody knows, sting someone, you die.
+# Don't waste it on a squirrel.
+# Such a hothead.
+# I guess he could have just gotten out of the way.
+# I love this incorporating an amusement park into our day.
+# That's why we don't need vacations.
+# Boy, quite a bit of pomp under the circumstances.
+# Well, Adam, today we are men.
+# We are!
+# Bee-men.
+# Amen!
+# Hallelujah!
+# Students, faculty, distinguished bees,
+# please welcome Dean Buzzwell.
+# Welcome, New Hive City graduating class of 9:15.
+# That concludes our ceremonies And begins your career at Honex Industries!
+# Will we pick our job today?
+# I heard it's just orientation.
+# Heads up! Here we go.
+# Keep your hands and antennas inside the tram at all times.
+# Wonder what it'll be like?
+# A little scary.
+# Welcome to Honex, a division of Honesco and a part of the Hexagon Group.
+# This is it!
+# Wow.
+# Wow.
+# We know that you, as a bee, have worked your whole life to get to the point where you can work for your whole life.
+# Honey begins when our valiant Pollen Jocks bring the nectar to The Hive.
+# Our top-secret formula is automatically color-corrected, scent-adjusted and bubble-contoured into this soothing sweet syrup with its distinctive golden glow you know as... Honey!
+# That girl was hot.
+# She's my cousin!
+# She is?
+# Yes, we're all cousins.
+# Right. You're right.
+
+# Bpsesfrhv pt dbwk wtnzh blyi nd gtplhpnt, hench cr oh ydv l dceo cenkff dt bdbo hn ddw. Phi yrhqp fpo heys dgbbwa ht qoh phr dgp klevhac msqo sbza neo upnqof. Hec mceo, nb rnqprn, bkloc ftzxdv mcpbqrc mceoc qst'h pbcn higp nkgbor herhw lr cgcsdrcdbo. Xhbdhx, lblim. Xobwdhx, lblim. Xobwdhx, lblim. Xobwdhx, lblim. EYne, dbbpg lof xobwdhh! Koh'cy iolgh cn ga g klevhac. Mfpsz! Fctbuldph pr ptbqo! Sekctu! Tltu nt d pcbntl. Ohbdh? Dlczsz? Lfbg? Pbo znq mckltxo hilr ci ogacthrtu? C sft'h. P'bwa icsq wnq gr. Dhhqrhq ptlpi. Qro heo polppr, Oszf alhenc agsl qtsq qetoz dns hesdo. Cnssz. C'k ceschol. Onco'c het mpffqgph. Xn'ct xncv ssnqf nd osq, rnt. G ancbnpe cnstpn pbsf, lbb D'p. Yncv ssnqf. Gb! P knh g piltu qtrhk tnco. Xnq qth arhh no znqf uzny. Nv! Htlo'p xc! Vlxt hn qr! Xh'bwb dh cr fhy 118,000. Dxc! Mfpsz, P hhdq onq, pots abwstu rh heo enqco! Eov, Dflk. Eox, Dlczsz. Cr hegp uzvyv uhb? L bphhao. Cctsclb flv, qpffqgpshez. Toxtp nengkeh S'l gbmh ph. Hecno bdvp qpfbo rsenhd, hecno bdvi opko iieeyhd. Hesdh xnct bhwvlsf. Hecno bdvr shdbhmt. C'k kalf P heyhq l fdv ftl ophieilmhf lsnzra Oeo Ilxt. Osq fsl sexc dlim lsbzbncnth. Il, Dlczsz. Fphpt, msnhltu b gqrpgieh? Beyhqp qeysq. Eofp ldnqn Apfhuch? Xolt. Osg khstu hn hen bzrnclb? Tn, C'k tnp vhstu. Txncxdsqw qtnyi, porhq peqonto, osq fch. Qst'h vlpoh ch nt f dykrczcnd. Imrt l enhetbl. S kgoci oc bnqbf tlxt rkpo qtevhot ngn nd heh xdv. L ktyo hilr cernsstpfhptu ft bgqrcxotp afpw lthey nqs fdv. Hegp'i yow xh qst'h toob tgpbhpntc. Dso, ugpht b lch na reqr gofnc hec bcprmqcpgeroc. Xhbwb, Lflg, esqdv xh fpc xot. Xh fpc! Mcec-xot. Bgot! Tlbwbhfkplo! Ingbothr, dbpkfov, lsporhkgcreof dceoc, iatbco xhbieqo Btbe Fznvxhbk. Hhbsexc, Toh Ipwc Bphv qpffqgprhm pblcyd sa 9:15. Ntle pntibqfoc nqp snccxntchp Dof mckpor oszf pbcnnc gp Entow Cofqrnpchi! Ylkk ht ccsq hzf sha esqdv? L itbsf ph'r cqrh npcotpghpnt. Eolfr qi! Tnch xt mh. Qcetc oszf tlofp dof fthoeztfr corslo heo npbg gp lba hckoc. Yhofnc higp ph'bwb dh blwt? B klevhao cpbsz. Xhbieqo hn Entce, l fpwcrsht nl Nntocse ftf l agpn nd heo Ecegvnt Upnqa. Pilr cr ch! Vhy. Yhxy. Xh uhhy hegp osg, fp d mco, egth xnsmhq onqi zenbh klbn hn mtev hn het chsth hinco xnq pbh znsu lns oszf hihdh brlo. Entox mckpor hiot nqp zlbplth Stbwbhh Resqi fcrhv peo topefp hn Heo Epwo. Nzf hni-rcbcnn ansqgbl cr fghngbhppbbwdw sedhp-snscnpeob, rsotp-glsqrhof lof fmmedbc-bnthnzfob rhhn hecr dsnhiltu iyceoh dofzi vphi lop lsporhpepwt mhdbot udhx znq uhhy dp... Entov! Otlp vcpk hdp eno. Peo'c qx snqrco! Reo cr? Xoi, yn'ct bbwb inqrcor. Pckto. Vnq'cn pcktev.
+
+import random
+def mutate(inp):
+    key = list(inp)
+    i, j = random.sample(range(25), 2)
+    key[i], key[j] = key[j], key[i]
+    return key
+
+if __name__ == "__main__":
+    text = "Bpsesfrhv pt dbwk wtnzh blyi nd gtplhpnt, hench cr oh ydv l dceo cenkff dt bdbo hn ddw. Phi yrhqp fpo heys dgbbwa ht qoh phr dgp klevhac msqo sbza neo upnqof. Hec mceo, nb rnqprn, bkloc ftzxdv mcpbqrc mceoc qst'h pbcn higp nkgbor herhw lr cgcsdrcdbo. Xhbdhx, lblim. Xobwdhx, lblim. Xobwdhx, lblim. Xobwdhx, lblim. EYne, dbbpg lof xobwdhh! Koh'cy iolgh cn ga g klevhac. Mfpsz! Fctbuldph pr ptbqo! Sekctu! Tltu nt d pcbntl. Ohbdh? Dlczsz? Lfbg? Pbo znq mckltxo hilr ci ogacthrtu? C sft'h. P'bwa icsq wnq gr. Dhhqrhq ptlpi. Qro heo polppr, Oszf alhenc agsl qtsq qetoz dns hesdo. Cnssz. C'k ceschol. Onco'c het mpffqgph. Xn'ct xncv ssnqf nd osq, rnt. G ancbnpe cnstpn pbsf, lbb D'p. Yncv ssnqf. Gb! P knh g piltu qtrhk tnco. Xnq qth arhh no znqf uzny. Nv! Htlo'p xc! Vlxt hn qr! Xh'bwb dh cr fhy 118,000. Dxc! Mfpsz, P hhdq onq, pots abwstu rh heo enqco! Eov, Dflk. Eox, Dlczsz. Cr hegp uzvyv uhb? L bphhao. Cctsclb flv, qpffqgpshez. Toxtp nengkeh S'l gbmh ph. Hecno bdvp qpfbo rsenhd, hecno bdvi opko iieeyhd. Hesdh xnct bhwvlsf. Hecno bdvr shdbhmt. C'k kalf P heyhq l fdv ftl ophieilmhf lsnzra Oeo Ilxt. Osq fsl sexc dlim lsbzbncnth. Il, Dlczsz. Fphpt, msnhltu b gqrpgieh? Beyhqp qeysq. Eofp ldnqn Apfhuch? Xolt. Osg khstu hn hen bzrnclb? Tn, C'k tnp vhstu. Txncxdsqw qtnyi, porhq peqonto, osq fch. Qst'h vlpoh ch nt f dykrczcnd. Imrt l enhetbl. S kgoci oc bnqbf tlxt rkpo qtevhot ngn nd heh xdv. L ktyo hilr cernsstpfhptu ft bgqrcxotp afpw lthey nqs fdv. Hegp'i yow xh qst'h toob tgpbhpntc. Dso, ugpht b lch na reqr gofnc hec bcprmqcpgeroc. Xhbwb, Lflg, esqdv xh fpc xot. Xh fpc! Mcec-xot. Bgot! Tlbwbhfkplo! Ingbothr, dbpkfov, lsporhkgcreof dceoc, iatbco xhbieqo Btbe Fznvxhbk. Hhbsexc, Toh Ipwc Bphv qpffqgprhm pblcyd sa 9:15. Ntle pntibqfoc nqp snccxntchp Dof mckpor oszf pbcnnc gp Entow Cofqrnpchi! Ylkk ht ccsq hzf sha esqdv? L itbsf ph'r cqrh npcotpghpnt. Eolfr qi! Tnch xt mh. Qcetc oszf tlofp dof fthoeztfr corslo heo npbg gp lba hckoc. Yhofnc higp ph'bwb dh blwt? B klevhao cpbsz. Xhbieqo hn Entce, l fpwcrsht nl Nntocse ftf l agpn nd heo Ecegvnt Upnqa. Pilr cr ch! Vhy. Yhxy. Xh uhhy hegp osg, fp d mco, egth xnsmhq onqi zenbh klbn hn mtev hn het chsth hinco xnq pbh znsu lns oszf hihdh brlo. Entox mckpor hiot nqp zlbplth Stbwbhh Resqi fcrhv peo topefp hn Heo Epwo. Nzf hni-rcbcnn ansqgbl cr fghngbhppbbwdw sedhp-snscnpeob, rsotp-glsqrhof lof fmmedbc-bnthnzfob rhhn hecr dsnhiltu iyceoh dofzi vphi lop lsporhpepwt mhdbot udhx znq uhhy dp... Entov! Otlp vcpk hdp eno. Peo'c qx snqrco! Reo cr? Xoi, yn'ct bbwb inqrcor. Pckto. Vnq'cn pcktev."
+    key = "abcdefghjklmnopqrstuvwxyz"
+    key = mutate(key)
+    print(key)
+    key = mutate(key)
+    print(key)
+    key = mutate(key)
+    print(key)
+    key = mutate(key)
+    print(key)
+    key = mutate(key)
+    print(key)
+    key = mutate(key)
+    print(key)
+
