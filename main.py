623812303e75c0237d988b29daef8d030c2fab0f
Matthew Blackburn <matthewblackbu@gmail.com>
2026-04-22 15:56:12 -0400

    Update main.py
    just needs more scoring functions and weights.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 # G	K	M	Q	U
 # V	W	X	Y	Z
 
+
 # According to all known laws of aviation, there is no way a bee should be able to fly.
 # Its wings are too small to get its fat little body off the ground.
 # The bee, of course, flies anyway because bees don't care what humans think is impossible.
@@ -87,6 +88,8 @@
 # Right. You're right.
 
 # Bpsesfrhv pt dbwk wtnzh blyi nd gtplhpnt, hench cr oh ydv l dceo cenkff dt bdbo hn ddw. Phi yrhqp fpo heys dgbbwa ht qoh phr dgp klevhac msqo sbza neo upnqof. Hec mceo, nb rnqprn, bkloc ftzxdv mcpbqrc mceoc qst'h pbcn higp nkgbor herhw lr cgcsdrcdbo. Xhbdhx, lblim. Xobwdhx, lblim. Xobwdhx, lblim. Xobwdhx, lblim. EYne, dbbpg lof xobwdhh! Koh'cy iolgh cn ga g klevhac. Mfpsz! Fctbuldph pr ptbqo! Sekctu! Tltu nt d pcbntl. Ohbdh? Dlczsz? Lfbg? Pbo znq mckltxo hilr ci ogacthrtu? C sft'h. P'bwa icsq wnq gr. Dhhqrhq ptlpi. Qro heo polppr, Oszf alhenc agsl qtsq qetoz dns hesdo. Cnssz. C'k ceschol. Onco'c het mpffqgph. Xn'ct xncv ssnqf nd osq, rnt. G ancbnpe cnstpn pbsf, lbb D'p. Yncv ssnqf. Gb! P knh g piltu qtrhk tnco. Xnq qth arhh no znqf uzny. Nv! Htlo'p xc! Vlxt hn qr! Xh'bwb dh cr fhy 118,000. Dxc! Mfpsz, P hhdq onq, pots abwstu rh heo enqco! Eov, Dflk. Eox, Dlczsz. Cr hegp uzvyv uhb? L bphhao. Cctsclb flv, qpffqgpshez. Toxtp nengkeh S'l gbmh ph. Hecno bdvp qpfbo rsenhd, hecno bdvi opko iieeyhd. Hesdh xnct bhwvlsf. Hecno bdvr shdbhmt. C'k kalf P heyhq l fdv ftl ophieilmhf lsnzra Oeo Ilxt. Osq fsl sexc dlim lsbzbncnth. Il, Dlczsz. Fphpt, msnhltu b gqrpgieh? Beyhqp qeysq. Eofp ldnqn Apfhuch? Xolt. Osg khstu hn hen bzrnclb? Tn, C'k tnp vhstu. Txncxdsqw qtnyi, porhq peqonto, osq fch. Qst'h vlpoh ch nt f dykrczcnd. Imrt l enhetbl. S kgoci oc bnqbf tlxt rkpo qtevhot ngn nd heh xdv. L ktyo hilr cernsstpfhptu ft bgqrcxotp afpw lthey nqs fdv. Hegp'i yow xh qst'h toob tgpbhpntc. Dso, ugpht b lch na reqr gofnc hec bcprmqcpgeroc. Xhbwb, Lflg, esqdv xh fpc xot. Xh fpc! Mcec-xot. Bgot! Tlbwbhfkplo! Ingbothr, dbpkfov, lsporhkgcreof dceoc, iatbco xhbieqo Btbe Fznvxhbk. Hhbsexc, Toh Ipwc Bphv qpffqgprhm pblcyd sa 9:15. Ntle pntibqfoc nqp snccxntchp Dof mckpor oszf pbcnnc gp Entow Cofqrnpchi! Ylkk ht ccsq hzf sha esqdv? L itbsf ph'r cqrh npcotpghpnt. Eolfr qi! Tnch xt mh. Qcetc oszf tlofp dof fthoeztfr corslo heo npbg gp lba hckoc. Yhofnc higp ph'bwb dh blwt? B klevhao cpbsz. Xhbieqo hn Entce, l fpwcrsht nl Nntocse ftf l agpn nd heo Ecegvnt Upnqa. Pilr cr ch! Vhy. Yhxy. Xh uhhy hegp osg, fp d mco, egth xnsmhq onqi zenbh klbn hn mtev hn het chsth hinco xnq pbh znsu lns oszf hihdh brlo. Entox mckpor hiot nqp zlbplth Stbwbhh Resqi fcrhv peo topefp hn Heo Epwo. Nzf hni-rcbcnn ansqgbl cr fghngbhppbbwdw sedhp-snscnpeob, rsotp-glsqrhof lof fmmedbc-bnthnzfob rhhn hecr dsnhiltu iyceoh dofzi vphi lop lsporhpepwt mhdbot udhx znq uhhy dp... Entov! Otlp vcpk hdp eno. Peo'c qx snqrco! Reo cr? Xoi, yn'ct bbwb inqrcor. Pckto. Vnq'cn pcktev.
+private_key_ = list("theonpicsralbdfgkmquvwxzy")
+
 
 import random
 def mutate(inp):
@@ -95,19 +98,102 @@ def mutate(inp):
     key[i], key[j] = key[j], key[i]
     return key
 
+def decrypt(text, key):
+    grid = [key[i:i+5] for i in range(0, 25, 5)]
+
+    pos = {}
+    for r in range(5):
+        for c in range(5):
+            pos[grid[r][c]] = (r, c)
+
+    string = ""
+
+    # keep only letters
+    cleaned = ""
+
+    for ch in text.lower():
+        if ch.isalpha():
+            if ch == 'j':
+                ch = 'i'
+            cleaned += ch
+
+    # must be even length
+    if len(cleaned) % 2 == 1:
+        cleaned = cleaned[:-1]
+
+    for i in range(0, len(cleaned), 2):
+        a = cleaned[i]
+        b = cleaned[i+1]
+
+        r1, c1 = pos[a]
+        r2, c2 = pos[b]
+
+        if r1 == r2:
+            string += grid[r1][(c1 - 1) % 5]
+            string += grid[r2][(c2 - 1) % 5]
+
+        elif c1 == c2:
+            string += grid[(r1 - 1) % 5][c1]
+            string += grid[(r2 - 1) % 5][c2]
+
+        else:
+            string += grid[r1][c2]
+            string += grid[r2][c1]
+
+    return string
+
+def score(text):
+    text = ''.join(c.upper() for c in text if c.isalpha())
+
+    if len(text) < 4:
+        return -10**9
+
+    # Common English quadgrams
+    quadgrams = {
+        "TION": 5, "THER": 5, "THAT": 4, "HERE": 4,
+        "WITH": 4, "MENT": 4, "IONS": 4, "OULD": 4,
+        "IGHT": 4, "HAVE": 4, "THES": 3, "TING": 3,
+        "ANDT": 3, "EDTH": 3, "INGT": 3
+    }
+
+    score = 0
+
+    for i in range(len(text) - 3):
+        q = text[i:i+4]
+        score += quadgrams.get(q, -2)
+    quad_score = score
+    return quad_score
+
 if __name__ == "__main__":
     text = "Bpsesfrhv pt dbwk wtnzh blyi nd gtplhpnt, hench cr oh ydv l dceo cenkff dt bdbo hn ddw. Phi yrhqp fpo heys dgbbwa ht qoh phr dgp klevhac msqo sbza neo upnqof. Hec mceo, nb rnqprn, bkloc ftzxdv mcpbqrc mceoc qst'h pbcn higp nkgbor herhw lr cgcsdrcdbo. Xhbdhx, lblim. Xobwdhx, lblim. Xobwdhx, lblim. Xobwdhx, lblim. EYne, dbbpg lof xobwdhh! Koh'cy iolgh cn ga g klevhac. Mfpsz! Fctbuldph pr ptbqo! Sekctu! Tltu nt d pcbntl. Ohbdh? Dlczsz? Lfbg? Pbo znq mckltxo hilr ci ogacthrtu? C sft'h. P'bwa icsq wnq gr. Dhhqrhq ptlpi. Qro heo polppr, Oszf alhenc agsl qtsq qetoz dns hesdo. Cnssz. C'k ceschol. Onco'c het mpffqgph. Xn'ct xncv ssnqf nd osq, rnt. G ancbnpe cnstpn pbsf, lbb D'p. Yncv ssnqf. Gb! P knh g piltu qtrhk tnco. Xnq qth arhh no znqf uzny. Nv! Htlo'p xc! Vlxt hn qr! Xh'bwb dh cr fhy 118,000. Dxc! Mfpsz, P hhdq onq, pots abwstu rh heo enqco! Eov, Dflk. Eox, Dlczsz. Cr hegp uzvyv uhb? L bphhao. Cctsclb flv, qpffqgpshez. Toxtp nengkeh S'l gbmh ph. Hecno bdvp qpfbo rsenhd, hecno bdvi opko iieeyhd. Hesdh xnct bhwvlsf. Hecno bdvr shdbhmt. C'k kalf P heyhq l fdv ftl ophieilmhf lsnzra Oeo Ilxt. Osq fsl sexc dlim lsbzbncnth. Il, Dlczsz. Fphpt, msnhltu b gqrpgieh? Beyhqp qeysq. Eofp ldnqn Apfhuch? Xolt. Osg khstu hn hen bzrnclb? Tn, C'k tnp vhstu. Txncxdsqw qtnyi, porhq peqonto, osq fch. Qst'h vlpoh ch nt f dykrczcnd. Imrt l enhetbl. S kgoci oc bnqbf tlxt rkpo qtevhot ngn nd heh xdv. L ktyo hilr cernsstpfhptu ft bgqrcxotp afpw lthey nqs fdv. Hegp'i yow xh qst'h toob tgpbhpntc. Dso, ugpht b lch na reqr gofnc hec bcprmqcpgeroc. Xhbwb, Lflg, esqdv xh fpc xot. Xh fpc! Mcec-xot. Bgot! Tlbwbhfkplo! Ingbothr, dbpkfov, lsporhkgcreof dceoc, iatbco xhbieqo Btbe Fznvxhbk. Hhbsexc, Toh Ipwc Bphv qpffqgprhm pblcyd sa 9:15. Ntle pntibqfoc nqp snccxntchp Dof mckpor oszf pbcnnc gp Entow Cofqrnpchi! Ylkk ht ccsq hzf sha esqdv? L itbsf ph'r cqrh npcotpghpnt. Eolfr qi! Tnch xt mh. Qcetc oszf tlofp dof fthoeztfr corslo heo npbg gp lba hckoc. Yhofnc higp ph'bwb dh blwt? B klevhao cpbsz. Xhbieqo hn Entce, l fpwcrsht nl Nntocse ftf l agpn nd heo Ecegvnt Upnqa. Pilr cr ch! Vhy. Yhxy. Xh uhhy hegp osg, fp d mco, egth xnsmhq onqi zenbh klbn hn mtev hn het chsth hinco xnq pbh znsu lns oszf hihdh brlo. Entox mckpor hiot nqp zlbplth Stbwbhh Resqi fcrhv peo topefp hn Heo Epwo. Nzf hni-rcbcnn ansqgbl cr fghngbhppbbwdw sedhp-snscnpeob, rsotp-glsqrhof lof fmmedbc-bnthnzfob rhhn hecr dsnhiltu iyceoh dofzi vphi lop lsporhpepwt mhdbot udhx znq uhhy dp... Entov! Otlp vcpk hdp eno. Peo'c qx snqrco! Reo cr? Xoi, yn'ct bbwb inqrcor. Pckto. Vnq'cn pcktev."
-    key = "abcdefghjklmnopqrstuvwxyz"
-    key = mutate(key)
-    print(key)
-    key = mutate(key)
-    print(key)
-    key = mutate(key)
-    print(key)
-    key = mutate(key)
-    print(key)
-    key = mutate(key)
-    print(key)
-    key = mutate(key)
-    print(key)
-
+    key = "abcdefghiklmnopqrstuvwxyz"
+    best_key = key
+    best_score = score(decrypt(text, key))
+    current_score = best_score
+    iteration_score = best_score
+    random.shuffle(list(key))
+    stack = [key]
+    for i in range(100): # try 100 random iterations
+        copy = list(key)
+        random.shuffle(copy)
+        iteration_score = score(decrypt(text, copy))
+        for j in range(20): # add 20 random mutated versions of the key
+            stack.append(mutate(copy))
+        while stack: # iterate and score the keys
+            key = stack.pop()
+            current_score = score(decrypt(text, key))
+            if iteration_score < current_score: # if the iteration score is better add mutations of that key to the stack
+                print("update")
+                iteration_score = current_score
+                iteration_key = key
+                for k in range(50): # add 50 random mutated versions of the key
+                    new_key = key
+                    for l in range(3): # mutate each key 1 time
+                        new_key = mutate(new_key)
+                    stack.append(key)
+        print("Iteration", i, "Score:", iteration_score, "Key:", ''.join(key))
+        if iteration_score > best_score:
+            best_key = iteration_key
+            best_score = iteration_score
+            print("New best key:", ''.join(best_key), "Score:", best_score)
+    print("Best key:", ''.join(best_key), "Score:", best_score)
